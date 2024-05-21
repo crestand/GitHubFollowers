7c70d5259127fbf057170b94705338bd0ec83d83
Eren Koc <crestand@outlook.com>
2024-05-21 03:23:53 +0300

    Created navigation base

---
+++ GitHubFollowers/FavoritesListVC.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritesListVC.swift
+//  GitHubFollowers
+//
+//  Created by Hamza Eren Koc on 21.05.2024.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+    
+
+   
+
+}
---
+++ GitHubFollowers/SearchVC.swift
@@ -0,0 +1,19 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by Hamza Eren Koc on 21.05.2024.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+
+
+}
