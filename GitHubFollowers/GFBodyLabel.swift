bd7f26f3f68b743dbf737554676dd29c172d96dd
Eren Koc <crestand@outlook.com>
2024-05-21 21:33:51 +0300

    Created custom alert view

---
+++ GitHubFollowers/GFBodyLabel.swift
@@ -0,0 +1,39 @@
+//
+//  GFBodyLabel.swift
+//  GitHubFollowers
+//
+//  Created by Hamza Eren Koc on 21.05.2024.
+//
+
+import UIKit
+
+class GFBodyLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        
+        configure()
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        textColor = .secondaryLabel
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+        
+    }
+}
